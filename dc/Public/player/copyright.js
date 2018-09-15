776ad76d1cafcf9afbec3f945ce3d14fa67cb4b7
lbb1996 <42107314+lbb1996@users.noreply.github.com>
2018-09-15 15:06:09 +0800

    Add files via upload

---
+++ dc/Public/player/copyright.js
@@ -0,0 +1,2 @@
+document.write('<iframe class="embed-responsive-item" src="http://cdn.feifeicms.co/player/3.0/copyright.html?'+cms_player.copyright+'" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>');
+window.setTimeout("window.location='"+cms_player.url+"'",(cms_player.copyright*1000));
---
+++ dc/Public/player/yugao.js
@@ -0,0 +1,1 @@
+document.write('<iframe class="embed-responsive-item" src="http://cdn.feifeicms.co/player/3.3/?type=yugao&u='+cms_player.url+'" width="100%" height="100%" frameborder="0" scrolling="no" allowfullscreen="true"></iframe>');
